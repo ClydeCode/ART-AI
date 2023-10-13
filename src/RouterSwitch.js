86c8c2af876e69522526f2df8029470844a60d37
Clyde <noneclyde@gmail.com>
2023-10-13 12:30:48 +0300

    Add RegisterPage

--- src/RouterSwitch.js
+++ src/RouterSwitch.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PromptPage from './pages/PromptPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 export default function RouterSwitch() {
     return (
@@ -10,6 +11,7 @@ export default function RouterSwitch() {
                 <Route path='/' element= { <HomePage /> } />
                 <Route path='/prompt' element= { <PromptPage /> } />
                 <Route path='/login' element= { <LoginPage /> } />
+                <Route path='/register' element= { <RegisterPage /> } />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,49 @@
+import { Box, Button, Container, Link, Paper, Stack, TextField, Typography } from "@mui/material";
+import DeviceHubIcon from '@mui/icons-material/DeviceHub';
+import Header from "../components/Header";
+
+export default function RegisterPage() {
+    return (
+        <div>
+            <Header />
+            <Container 
+                maxWidth='sm'
+            >
+                <Paper elevation={1}>
+                    <Box padding={10}>
+                        <Stack gap={4}>
+                            <Box display='flex' alignItems='center' justifyContent='center'>
+                                <DeviceHubIcon sx={{ mr: 1 }} />
+                                <Typography 
+                                    variant='h5'
+                                    fontFamily='Montserrat'
+                                    fontSize='1.9rem'
+                                    sx={{
+                                        cursor: 'default',
+                                        userSelect: 'none'
+                                    }}
+                                >
+                                    ART-AI
+                                </Typography>
+                            </Box>
+                            <TextField id='username-field' label='Username' variant='outlined' color='primary'/>
+                            <TextField id='email-field' label='Email' variant='outlined' color='primary'/>
+                            <TextField id='password-field' label='Password' variant='outlined' color='primary'/>
+                            <TextField id='confirm-password-field' label='Confirm Password' variant='outlined' color='primary'/>
+                            <Button 
+                                variant='contained'
+                                sx={{
+                                    height: '45px',
+                                    mt: 2
+                                }}
+                            >
+                                Register
+                            </Button>
+                            <Link href='/login' underline='hover' alignSelf='center' marginTop={-2}>Already have an account?</Link>
+                        </Stack>
+                    </Box>
+                </Paper>
+            </Container>
+        </div>
+    );
+}
