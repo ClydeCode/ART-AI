48f162c388ffe03e4cd7cb7e9453641636ab212f
Klaidas Kukoris <noneclyde@gmail.com>
2023-10-17 12:59:09 +0300

    Resolve conflicts

--- src/components/Header.js
+++ src/components/Header.js
@@ -75,17 +75,17 @@ export default function Header() {
                     </Link>
                     <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
+                            size='large'
+                            aria-label='account of current user'
+                            aria-controls='menu-appbar'
+                            aria-haspopup='true'
                             onClick={handleOpenNavMenu}
-                            color="inherit"
+                            color='inherit'
                         >
                             <MenuIcon />
                         </IconButton>
                         <Menu
-                            id="menu-appbar"
+                            id='menu-appbar'
                             anchorEl={anchorElNav}
                             anchorOrigin={{
                                 vertical: 'bottom',
