c4371f6aad036fe7521ed818970ef10c58ae1ae7
Klaidas Kukoris <noneclyde@gmail.com>
2023-10-17 14:55:25 +0300

    Add GalleryPage prototype

--- src/RouterSwitch.js
+++ src/RouterSwitch.js
@@ -3,12 +3,14 @@ import HomePage from './pages/HomePage';
 import PromptPage from './pages/PromptPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import GalleryPage from './pages/GalleryPage';
 
 export default function RouterSwitch() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element= { <HomePage /> } />
+                <Route path='/gallery' element= { <GalleryPage /> } />
                 <Route path='/prompt' element= { <PromptPage /> } />
                 <Route path='/login' element= { <LoginPage /> } />
                 <Route path='/register' element= { <RegisterPage /> } />
