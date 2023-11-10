5fda3da5f05c9946be9d074f2e897fc74a49b974
Clyde <noneclyde@gmail.com>
2023-11-10 22:06:21 +0200

    Add RegisterRequest function in UserAPI

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -4,7 +4,18 @@ export async function AuthRequest(username, password) {
     const response = instance.post('/authentication/login', {
         UserName: username,
         Password: password
-    })
+    });
+
+    return await response;
+}
+
+export async function RegisterRequest(username, email, password, confirmPassword) {
+    const response = instance.post('/authentication/register', {
+        UserName: username,
+        Email: email,
+        Password: password,
+        ConfirmPassword: confirmPassword
+    });
 
     return await response;
 }
